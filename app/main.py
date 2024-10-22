a9cb4266dad29cf74da1faf2a31f0a1a0d982753
José Rolando <97265804+GRJR1325@users.noreply.github.com>
2024-10-22 22:17:01 +0000

    UPDATE app

--- app/main.py
+++ app/main.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hola, Mundo desde fastAPI!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=6060)
